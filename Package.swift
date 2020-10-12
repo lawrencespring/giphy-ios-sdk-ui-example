338e9bba5aa4d0915723812f8d119a4e559a95fe
Daniele <me@danieleforlani.net>
2020-10-12 17:07:21 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,23 +15,21 @@ let package = Package(
             name: "GiphyUISDK",
             targets: ["GiphyCoreSDKWrapper"])
     ],
-    dependencies: [
+    dependencies: [.package(url: "https://github.com/pinterest/PINCache.git", from: "1.2.0"),
+                   .package(url: "https://github.com/onmyway133/DeepDiff.git", from: "2.3.1"),
+                   .package(url: "https://github.com/lawrencespring/WebP", from: "0.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GiphyCoreSDKWrapper",
-            dependencies: [.package(url: "https://github.com/pinterest/PINCache.git", from: "1.2.0"),
-                           .package(url: "https://github.com/onmyway133/DeepDiff.git", from: "2.3.1"),
-                           .package(url: "https://github.com/lawrencespring/WebP", from: "0.0.2")],
+            dependencies: ["PINCache", "DeepDiff", "WebP"],
             path: "GiphyCoreSDKWrapper"
         ),
         .target(
             name: "GiphyUISDKWrapper",
-            dependencies: [.package(url: "https://github.com/pinterest/PINCache.git", from: "1.2.0"),
-                           .package(url: "https://github.com/onmyway133/DeepDiff.git", from: "2.3.1"),
-                           .package(url: "https://github.com/lawrencespring/WebP", from: "0.0.2")],
+            dependencies: ["PINCache", "DeepDiff", "WebP"],
             path: "GiphyCoreSDKWrapper"
         ),
        .binaryTarget(
