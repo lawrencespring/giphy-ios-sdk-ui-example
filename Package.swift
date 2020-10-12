f04aa6e0e182a971cc51d2430b94747f0e47b710
Daniele <me@danieleforlani.net>
2020-10-12 17:44:00 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["GiphyCoreSDKWrapper"]),
         .library(
             name: "GiphyUISDK",
-            targets: ["GiphyUISDKWrapper"])
+            targets: ["GiphyUISDK"])
     ],
     dependencies: [.package(url: "https://github.com/pinterest/PINCache.git", from: "3.0.2"),
                    .package(url: "https://github.com/onmyway133/DeepDiff.git", from: "2.3.1"),
@@ -24,13 +24,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GiphyCoreSDKWrapper",
-            dependencies: ["PINCache", "DeepDiff", "WebP"],
+            dependencies: ["PINCache", "DeepDiff", "WebP", .target(name: "GiphyCoreSDK"), .target(name: "GiphyUISDK")],
             path: "GiphyCoreSDKWrapper"
-        ),
-        .target(
-            name: "GiphyUISDKWrapper",
-            dependencies: ["PINCache", "DeepDiff", "WebP"],
-            path: "GiphyUISDKWrapper"
+        ),path: "GiphyUISDKWrapper"
         ),
        .binaryTarget(
             name: "GiphyCoreSDK",
